d566b5e992ad426ec76049d39b2dc46f3034458a
dkm-della <66500209+dkm-della@users.noreply.github.com>
2020-06-06 17:54:34 +0200

    Add files via upload

--- RNA/examples/test_simple/test_simple.ino
+++ RNA/examples/test_simple/test_simple.ino
@@ -1,5 +1,6 @@
 #include <rna_v1.h>
 
+// Network instance
 RNA RNA1(2, 5, 1);
 
 void setup()
@@ -7,17 +8,20 @@ void setup()
 	Serial.begin(115200);
 	RNA1.printNetwork();
 	float x[]={0.02, 0.7465};
-	float y[] = { 1.085 };
-	for(int i = 0; i < 50000; i++)
-    {
+	float y[] = { 0.32415 };
+  Serial.println("   Training the Network    ");
+	for(int i = 0; i < 5000; i++)
+  {
     	float error = RNA1.train(x, y, 0.07);
-    }
-	Serial.println(*RNA1.predict(x));
+     //Serial.println(error, 5);
+  }
+  Serial.println("   Evaluating the Network    ");  
+  float x1[]={0.018, 0.74};
+	Serial.println(*RNA1.predict(x1), 5);
 }
 
 void loop()
 {
 	// To Do
 }
-
 
